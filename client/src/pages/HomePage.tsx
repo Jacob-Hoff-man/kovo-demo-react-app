0aa2945d451966fdb3c70374ca8a131d44b5a644
Jacob Hoffman <cubhoffman@gmail.com>
2024-12-05 16:07:52 -0500

    ADD: initial HomePage and ItemsPage component

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react'
+
+type HomePageProps = {}
+
+const HomePage = ({ }: HomePageProps): ReactElement => {
+    return (
+        <p>
+            Home Page
+        </p>
+    )
+}
+
+export default HomePage
---
+++ client/src/pages/ItemsPage.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react'
+
+type ItemsPageProps = {}
+
+const ItemsPage = ({ }: ItemsPageProps): ReactElement => {
+    return (
+        <p>
+            Items Page
+        </p>
+    )
+}
+
+export default ItemsPage
